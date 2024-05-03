b643b6d0e4bbe7eed07c0c2028bc49b0032a0c6c
Vlad <Vlad@DESKTOP-ICG4QVG>
2024-05-03 18:18:37 +0300

    Добавьте файлы проекта.

---
+++ Generator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Lightshot URL Generator";
+            Console.WriteLine("Lightshot URL Generator\n");
+            Console.Write("How many links do you want to generate >");
+            int linksCount = int.Parse(Console.ReadLine());
+
+            string urlChrs = "qwertyuiopasdfghjklzxcvbnm1234567890";
+            List<string> listUrls = new List<string>();
+
+            for (int i = 0;i<linksCount;i++)
+            {
+                char[] charMap = urlChrs.ToCharArray();
+                Random rand = new Random();
+                string output = "https://prntscr.com/";
+                for (int ii = 0; ii<6;ii++)
+                {
+                    char randChar = charMap[rand.Next(0, charMap.Length)];
+                    output += randChar;
+                }
+                listUrls.Add(output);
+            }
+
+            Console.WriteLine("\n\nYour URLs are:\n");
+
+            foreach (string url in listUrls)
+            {
+                Console.WriteLine(url);
+            }
+
+            Console.WriteLine("\nClick any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
